a917c24082c8250becfa4c7ebc970d11894af390
AkshatR <mailme.akshatraizada@gmail.com>
2022-05-15 20:32:02 +0530

    Initial commit

---
+++ src/com/gl/javafsd/dsa/gradedassignment2/constructionplanner/Main.java
@@ -0,0 +1,8 @@
+package com.gl.javafsd.dsa.gradedassignment2.constructionplanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ConstructionPlanner sample= new ConstructionPlanner();
+        sample.dayWiseWorkCalculator();
+    }
+}
